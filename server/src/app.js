06ec637d57f217ff4941d89458b3d937db13027f
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-06-23 10:33:59 -0700

    install mongodb and mongoose

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/posts");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function (callback) {
+  console.log("Connection Succeeded");
+});
+
 const app = express();
 app.use(morgan("combined"));
 app.use(bodyParser.json());
