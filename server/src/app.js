15ad3a6a1ca388b41bdbe53dff6c35efe348e554
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-06-23 00:35:03 -0700

    set up server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("combined"));
+app.use(bodyParser.json);
+app.use(cors());
+
+app.get("/posts", (req, res) => {
+  res.setEncoding([
+    {
+      title: "Hello World",
+      description: "Hi there!",
+    },
+  ]);
+});
+
+app.listen(process.env.PORT || 8081);
