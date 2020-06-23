6d2af848ff7fc0181948c95e82d1ec27295a9b91
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-06-23 14:07:11 -0700

    add new post from front end UI

--- client/services/PostsService.js
+++ client/services/PostsService.js
@@ -1,7 +1,11 @@
-import Api from "./api";
+import Api from "../services/api";
 
 export default {
   fetchPosts() {
     return Api().get("posts");
+  },
+
+  addPost(params) {
+    return Api().post("posts", params);
   }
 };
